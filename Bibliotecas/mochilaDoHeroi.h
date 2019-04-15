b59bfb2b868453fed8cf8e152868081a9db3ce57
Lucas Santiago <lu.santi.oli@gmail.com>
2019-04-15 20:32:10 -0300

    MochilaDoHeroi v1.2

--- Bibliotecas/mochilaDoHeroi.h
+++ Bibliotecas/mochilaDoHeroi.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 
 void apresentacao(){
@@ -56,6 +57,28 @@ Saida: float
   printf("\nPor favor escreva um numero: ");
   fflush(stdin);
   scanf("%f", &inputDeUsuario);
+
+  return inputDeUsuario;
+}
+
+float lerChar(char type){
+/*
+Lucas Santiago - 15/04/19
+Objetivo: ler um char escrito pelo usuario
+Entrada: opcional do programador - se enviar a letra 'l'
+sera retornado um valor minusculo e se colocado 'u' sera
+retornado um valor maiusculo, qualquer outro valor ira retornar
+o valor que o usuario digitou
+Saida: char
+*/
+  char inputDeUsuario;
+
+  printf("\nEscreva uma letra: ");
+  fflush(stdin);
+  scanf("%c", &inputDeUsuario);
+
+  if(type == 'l') inputDeUsuario = tolower(inputDeUsuario);
+  if(type == 'u') inputDeUsuario = toupper(inputDeUsuario);
 
   return inputDeUsuario;
 }
@@ -117,4 +140,92 @@ Saida: Apenas numeros positivos e inteiros (excluindo o zero)
     scanf("%d", &numero);
     if(numero <= 0) printf("\nValor invalido");
   }while(numero <= 0);
+}
+
+int fatorial(int num){
+/*
+Lucas Santiago - 13/04/19
+Objetivo: calcular o fatorial de um numero
+Entrada: numero inteiro
+Saida: numero inteiro
+*/
+    int fatorial = 1;
+
+    for(int i = num; i > 0; i--){
+      fatorial *= i;
+    }
+
+    return fatorial;
+}
+
+float potencia(float base, int expoente){
+/*
+Lucas Santiago - 13/04/19
+Objetivo: calcular potencia de um numero
+Entrada: numero para ser elevado, expoente
+Saida: resultado da potencia em float
+*/
+  float potencia = 1;
+  for(int i = 0; i < expoente; i++){
+    potencia *= base;
+  }
+  return potencia;
+}
+
+float maiorNumero(){
+/*
+Lucas Santiago - 13/04/19
+Objetivo: Calcular o maior entre varios numeros lidos
+Sem entrada
+Saida: maior numero
+*/
+  float maior,
+        inputDeUsuario;
+
+  const float flag = 0;
+
+  printf("\nEscreva um numero (0 para sair): ");
+  fflush(stdin);
+  scanf("%f", &inputDeUsuario);
+
+  maior = inputDeUsuario;
+
+  while(inputDeUsuario != flag){
+    printf("\nEscreva um numero (0 para sair): ");
+    fflush(stdin);
+    scanf("%f", &inputDeUsuario);
+
+    if(inputDeUsuario > maior && inputDeUsuario != flag) maior = inputDeUsuario;
+  }
+
+  return maior;
+}
+
+float menorNumero(){
+/*
+Lucas Santiago - 13/04/19
+Objetivo: Calcular o menor entre varios numeros lidos
+Sem entrada
+Saida: menor numero
+*/
+  float menor,
+        inputDeUsuario;
+
+  const float flag = 0;
+
+  printf("\nEscreva um numero (0 para sair): ");
+  fflush(stdin);
+  scanf("%f", &inputDeUsuario);
+
+  menor = inputDeUsuario;
+
+  while(inputDeUsuario != 0){
+    printf("\nEscreva um numero (0 para sair): ");
+    fflush(stdin);
+    scanf("%f", &inputDeUsuario);
+
+    if(inputDeUsuario < menor && inputDeUsuario != flag) menor = inputDeUsuario;
+  }
+
+  return menor;
 }
